c1e297f45907ea80e8a864e99417d6fcf7e2b598
Zahed <zahedm45@gmail.com>
2022-02-25 17:46:06 +0100

    ...

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.StringTokenizer;
 
 public class Assignment1 {
 
-
+    int jumps;
     public static void main(String[] args) throws IOException {
         new Assignment1().run();
     }
@@ -14,49 +13,78 @@ public class Assignment1 {
 
     private void run() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
         String[] str = in.readLine().split(" ");
 
-
-
         int n = Integer.parseInt(str[0]);
-        int strength = Integer.parseInt(str[1]);
+        jumps = Integer.parseInt(str[1]);
 
 
-        int[] a = new int[n];
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(in.readLine());
-
-
 
 
         for (int i = 0; i < n; i++) {
 
             String s = st.nextToken();
-            System.out.println(s + " and " + Integer.parseInt(s));
-            a[i] = Integer.parseInt(s);
+            arr[i] = Integer.parseInt(s);
         }
 
 
-/*        for (int i : a) {
-            System.out.println(i);
-        }*/
+
+
+        int initialStrength = arr[arr.length-1];
+        any(arr, initialStrength);
+    }
+
+    private void any(int[] arr, int initialStrength) {
 
 
 
-/*        int heightAndStrength = a[0] + strength;
-        int counter = 0;
+        for (int i = initialStrength; i > 0; i--) {
 
-
-        for (int i = 1; i < a.length-1; i++) {
-
-            if (heightAndStrength < i) {
-                heightAndStrength = a[i-1] + strength;
-                counter++;
+            int initJumps = isPossible(arr, i);
+            if (initJumps == jumps) {
+                System.out.println(i);
+                return;
             }
         }
 
 
-        System.out.println(counter);*/
+/*        if (initJumps == jumps) {
+            System.out.println(initialStrength);
+            return;
+
+        } else if (initJumps < jumps) {
+            initialStrength = initialStrength / 2;
+            any(arr, initialStrength);
+
+        } else {
+            initialStrength = initialStrength + (initialStrength / 2);
+            any(arr, initialStrength);
+        }*/
+
+
+    }
+
+    private int isPossible(int[] arr, int strength) {
+        int heightAndStrength = arr[0] + strength;
+        int jumps = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            //int currValue = arr[i];
+            if ( (heightAndStrength < arr[i] || i == arr.length -1) && ((i+strength) >= arr[i]) ) {
+                heightAndStrength = arr[i-1] + strength;
+                jumps++;
+
+            }
+
+            /*else if (i == arr.length -1) {
+                heightAndStrength = arr[i-1] + strength;
+                jumps++;
+            }*/
+        }
+
+        return jumps;
     }
 
 }
