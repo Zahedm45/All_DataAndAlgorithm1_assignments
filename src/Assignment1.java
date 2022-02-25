2039be6a1839b8d296f4921eaba45bd3e0c95512
Zahed <zahedm45@gmail.com>
2022-02-25 13:21:41 +0100

    fixed the bug

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.StringTokenizer;
 
 public class Assignment1 {
@@ -19,23 +20,43 @@ public class Assignment1 {
 
 
         int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
+        int strength = Integer.parseInt(str[1]);
 
 
         int[] a = new int[n];
         StringTokenizer st = new StringTokenizer(in.readLine());
 
-        System.out.println(n + " " + m);
+
+
+
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+
+            String s = st.nextToken();
+            System.out.println(s + " and " + Integer.parseInt(s));
+            a[i] = Integer.parseInt(s);
         }
 
 
-        for (int i : a) {
+/*        for (int i : a) {
             System.out.println(i);
+        }*/
+
+
+
+/*        int heightAndStrength = a[0] + strength;
+        int counter = 0;
+
+
+        for (int i = 1; i < a.length-1; i++) {
+
+            if (heightAndStrength < i) {
+                heightAndStrength = a[i-1] + strength;
+                counter++;
+            }
         }
 
 
+        System.out.println(counter);*/
     }
 
 }
