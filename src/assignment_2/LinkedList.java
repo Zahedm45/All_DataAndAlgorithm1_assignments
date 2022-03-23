6fae0d0cb8b7b7f5d4b052ca458d800eb65b7af6
Zahed <zahedm45@gmail.com>
2022-03-23 11:03:53 +0100

    Cleaned up

--- src/assignment_2/LinkedList.java
+++ src/assignment_2/LinkedList.java
@@ -1,24 +1,14 @@
 package assignment_2;
 
 public class LinkedList {
-    //private Node node;
-
-/*    private Node leftNode ;
-    private Node midNode;
-    private Node rightNode;*/
-
     private Node head;
     private Node tail;
     private Node midNode;
+    private int totalNodeSize = 0;
+
 
     private final static String MOVE_RIGHT = "right";
     private final static String MOVE_LEFT = "left";
-
-
-    private int leftSideNodeSize = 0;
-    private int rightSideNodeSize = 0;
-    private int totalNodeSize = 0;
-    //private int midNodeAt = 0;
 
     public void addRightNode(String value) {
         totalNodeSize += 1;
@@ -29,25 +19,12 @@ public class LinkedList {
         } else {
             tail.rightNode = new Node(tail, value, MOVE_RIGHT,null);
             tail = tail.rightNode;
-            rightSideNodeSize += 1;
-            //System.out.println("total size: " + totalNodeSize +" right size: "+ rightSideNodeSize +"  left: " + leftSideNodeSize);
             if (totalNodeSize > 2) {
                 if (totalNodeSize % 2 != 0) {
                     midNode = midNode.rightNode;
                 }
-
-
-/*                if (rightSideNodeSize > leftSideNodeSize + 1) {
-                    midNode = midNode.rightNode;
-                    rightSideNodeSize--;
-                    leftSideNodeSize++;
-                }*/
-
-
             }
-
         }
-
     }
 
 
@@ -59,7 +36,6 @@ public class LinkedList {
         } else {
             head.leftNode = new Node(null, value,MOVE_LEFT, head);
             head = head.leftNode;
-            leftSideNodeSize++;
 
             if (totalNodeSize > 1) {
                 if (totalNodeSize % 2 == 0) {
@@ -67,25 +43,17 @@ public class LinkedList {
                 }
             }
         }
-
     }
 
 
     private void addFirstNode(String value, String leftOrRight) {
         tail = new Node(null, value, leftOrRight, null);
         midNode = head = tail;
-
     }
-
-
-
-
 
 
     public Node extractMidNode() {
         Node temp = midNode;
-
-
         if (totalNodeSize == 1) {
             midNode = head = tail = null;
             totalNodeSize = 0;
@@ -94,8 +62,6 @@ public class LinkedList {
         } else if (totalNodeSize == 2) {
             head = midNode = midNode.rightNode;
             totalNodeSize = 1;
-            leftSideNodeSize = rightSideNodeSize = 0;
-
         } else if (totalNodeSize > 2) {
 
 
@@ -106,13 +72,9 @@ public class LinkedList {
 
             if (totalNodeSize % 2 == 0) {
                 midNode = midNode.leftNode;
-                leftSideNodeSize--;
-                rightSideNodeSize++;
+
             } else {
                 midNode = midNode.rightNode;
-                leftSideNodeSize++;
-                rightSideNodeSize--;
-
             }
         }
         return temp;
